c85a67d28fea20abc6b0e05ffb7ae666128995f8
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-02 09:46:52 +0100

    commit

--- vehiculo.py
+++ vehiculo.py
@@ -26,4 +26,23 @@ class Camioneta(Coche):
 camioneta=Camioneta("blanco", 4, 120, 3000, 1500)
 print(camioneta)
 
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo=tipo
+    def __str__(self):
+        return Vehiculo.__str__(self)+", su tipo es "+self.tipo
+    
+bicicleta=Bicicleta("azul", 2, "urbana")
+print(bicicleta)
 
+class Motocicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas, tipo)
+        self.velocidad=velocidad
+        self.cilindrada=cilindrada
+    def __str__(self):
+        return Vehiculo.__str__(self)+" su velocidad es "+str(self.velocidad)+" su cilindrada es "+str(self.cilindrada)
+
+motocicleta=Motocicleta("azul", 2, "urbana", 100, 500)
+print(motocicleta)
