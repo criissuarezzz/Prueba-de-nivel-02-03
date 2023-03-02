0485d39d3ae50377177147ea03b4a22e7d12b09d
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-02 09:51:16 +0100

    commit

--- vehiculo.py
+++ vehiculo.py
@@ -1,10 +1,17 @@
+import csv
+
 class Vehiculo():
     def __init__(self, color, ruedas):
         self.color=color
         self.ruedas=ruedas
     def __str__(self):
         return "El color es "+self.color+" y tiene "+str(self.ruedas)+" ruedas"
-    
+    def to_dict(self):
+        return {
+            'color': self.color,
+            'ruedas': self.ruedas
+        }
+
 class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada):
         super().__init__(color, ruedas)
@@ -12,9 +19,17 @@ class Coche(Vehiculo):
         self.cilindrada=cilindrada
     def __str__(self):
         return Vehiculo.__str__(self)+" su velocidad es "+str(self.velocidad)+" su cilindrada es "+str(self.cilindrada)
+    def to_dict(self):
+        return {
+            'color': self.color,
+            'ruedas': self.ruedas,
+            'velocidad': self.velocidad,
+            'cilindrada': self.cilindrada
+        }
     
 coche=Coche("rojo", 4, 150, 2000)
 print(coche)
+
 
 class Camioneta(Coche):
     def __init__(self, color, ruedas, velocidad, cilindrada, carga):
@@ -37,12 +52,13 @@ bicicleta=Bicicleta("azul", 2, "urbana")
 print(bicicleta)
 
 class Motocicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
-        super().__init__(color, ruedas, tipo)
+    def __init__(self, color, ruedas,tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas)
+        self.tipo=tipo
         self.velocidad=velocidad
         self.cilindrada=cilindrada
     def __str__(self):
-        return Vehiculo.__str__(self)+" su velocidad es "+str(self.velocidad)+" su cilindrada es "+str(self.cilindrada)
-
-motocicleta=Motocicleta("azul", 2, "urbana", 100, 500)
-print(motocicleta)
+        return Vehiculo.__str__(self)+", su tipo es "+self.tipo+", su velocidad es "+str(self.velocidad)+" y su cilindrada es "+str(self.cilindrada)
+    
+moto=Motocicleta("negra", 2, "urbana", 200, 1000)
+print(moto)
