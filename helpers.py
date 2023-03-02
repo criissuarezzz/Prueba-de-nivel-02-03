712cf893836a8e6737764d3e8d0eb934193e8de1
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-02 17:55:06 +0100

    commit

--- helpers.py
+++ helpers.py
@@ -7,3 +7,10 @@ def limpiar_pantalla():      # Función para limpiar la pantalla
         os.system('cls')
     else:
         os.system('clear')
+
+def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):    #longitud minima y maxima del texto, y mensaje opcional
+    print(mensaje) if mensaje else None #si mensaje es None, no se imprime nada, si no, se imprime el mensaje
+    while True:   
+        texto=input("Introduzca un texto: ")  #leemos el texto
+        if len(texto) >= longitud_min and len(texto) <= longitud_max:   #comprobamos que el texto cumple las condiciones y si es asi lo devolvemos
+            return texto
